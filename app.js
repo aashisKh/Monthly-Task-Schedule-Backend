fa982e6d8c71a407d76852d178a9d126b66199c3
Aashis Khanal <70253251+aashisKh@users.noreply.github.com>
2024-09-02 12:39:57 +0545

    fixed issue

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const cors = require("cors");
+const { connectToDatabase } = require("./database/DbConnection");
+const router = require("./routes/router");
+connectToDatabase();
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(express.json());
 
-const express = require('express')
-require('dotenv').config()
-const app = express()
-const cors = require('cors')
-const {connectToDatabase} = require('./database/DbConnection')
-const router = require('./routes/router')
-connectToDatabase()
-app.use(cors({
-    origin: "*"
-}))''
-app.use(express.json())
+app.use(router);
 
-app.use(router)
-
-app.listen(process.env.PORT,()=> {
-    console.log(`Server running on Port ${process.env.PORT} !!!`)
-})
-
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on Port ${process.env.PORT} !!!`);
+});
